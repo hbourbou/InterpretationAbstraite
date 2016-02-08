4139f6c932bf9beee801b981573c75db9631adb1
hbourbou <bourbouhh@gmail.com>
2016-02-08 12:12:32 +0100

    ntervalsWithErrors

--- multi/src/domains/intervalsWithError.ml
+++ multi/src/domains/intervalsWithError.ml
@@ -7,7 +7,7 @@ let parse_param _ = ()
 
 let fprint_help fmt = Format.fprintf fmt "Interval abstraction"
 
-type intervalleWithError = Bot | Itv of (Q.t * Q.t)*(Q.t*Q.t)
+type intervalleWithError = Bot | Itv of (float * float)*(float*float)
 (* Itv ((x,ex);(y,ey))*)
 type t = intervalleWithError
 
@@ -15,16 +15,16 @@ type t = intervalleWithError
 let fprint ff = function
   | Bot -> Format.fprintf ff "⊥"
   | Itv ((a, ea),(b,eb)) -> Format.fprintf ff "%s%s%s, %s%s; %s%s, %s%s%s"
-    (if Q.equal a Q.minus_inf then "(" else "[")
-   ("(")(Q.to_string a) (Q.to_string ea)(")")
-   ("(")(Q.to_string b) (Q.to_string eb) (")")
-   (if Q.equal b Q.inf then ")" else "]")
+    (if (classify_float a) = FP_infinite  then "(" else "[")
+   ("(")(string_of_float a) (string_of_float ea)(")")
+   ("(")(string_of_float b) (string_of_float eb) (")")
+   (if (classify_float b) = FP_infinite then ")" else "]")
 let order x y = match x, y with
   | Bot, _ -> true
   | _, Bot -> false
-  | Itv ((a, ea),(b,eb)), Itv ((c, ec),(d,ed)) -> Q.leq c a && Q.leq b d
+  | Itv ((a, ea),(b,eb)), Itv ((c, ec),(d,ed)) ->  c <= a &&  b <= d
 
-let top = Itv ((Q.minus_inf, Q.of_int 0),(Q.inf,Q.of_int 0))
+let top = Itv ((neg_infinity,  0.0),(infinity,0.0))
 let bottom = Bot
 let is_bottom x = x = Bot
 
@@ -32,10 +32,10 @@ let join x y = match x, y with
   | Bot, _ -> y
   | _, Bot -> x
   | Itv ((a, ea),(b,eb)), Itv ((c, ec),(d,ed))-> 
-			let e1 = if (Q.leq a c) then ea else ec
+			let e1 = if ( a <= c) then ea else ec
 			in 
-			let e2 = if (Q.geq b d) then eb else ed
-			in Itv ((Q.min a c,e1),( Q.max b d,e2))
+			let e2 = if ( b >= d) then eb else ed
+			in Itv ((min a c,e1),( max b d,e2))
 
 
 
@@ -43,37 +43,37 @@ let meet x y = match x, y with
   | Bot, _
   | _, Bot -> Bot
   | Itv ((a, ea),(b,eb)), Itv ((c, ec),(d,ed)) -> 
-			let e1 = if (Q.geq a c) then ea else ec
+			let e1 = if ( a >= c) then ea else ec
 			in 
-			let e2 = if (Q.leq b d) then eb else ed
-			in Itv ((Q.max a c,e1),( Q.min b d,e2))
+			let e2 = if (b <= d) then eb else ed
+			in Itv ((max a c,e1),( min b d,e2))
 let widening x y = match x, y with
   | Bot, _ -> y
   | _, Bot -> x
   | Itv ((a, ea),(b,eb)), Itv ((c, ec),(d,ed)) ->
-    let e = if Q.leq a c then (a,ea) else (Q.minus_inf,Q.of_int 0) in
-    let f = if Q.leq d b then (b,eb) else (Q.inf,Q.of_int 0) in
+    let e = if  a <= c then (a,ea) else (neg_infinity, 0.0) in
+    let f = if d  <= b then (b,eb) else (infinity, 0.0) in
     Itv (e, f)
 (* [mk_itv i1 i2] returns the interval (Itv ((i1,ei1),(i2,ei2))) when i1 <= i2,
    Bot otherwise. *)
 (*rounOffError n'est pas correcte mais elle crée un erreur*)
-let roundOffError x = Q.sub x  (Q.of_float (float (Q.to_int x)))
-let mk_itv i1 i2 = if Q.leq i1  i2 then 
-			let e1 = roundOffError i1 
+let roundOffError x = abs_float(x)*.(2.**(-53.)) 
+
+let sem_itv (q1,s1) (q2,s2) = if Q.leq q1  q2 then 
+			let e1 = roundOffError (float_of_string s1) 
 			in 
-			let e2 = roundOffError i2
+			let e2 = roundOffError (float_of_string s2) 
 			in
-			Itv ((i1, e1),(i2,e2)) 
+			Itv ((float_of_string s1, e1),(float_of_string s2,e2)) 
 		    else Bot
-let sem_itv n1 n2 = mk_itv n1 n2
 
 let sem_plus x y = match x, y with
   | Bot, _ -> Bot
   | _, Bot -> Bot
   |  Itv ((a, ea),(b,eb)), Itv ((c, ec),(d,ed))  -> 
-		let e = (Q.add a c, Q.add (Q.add ea ec) (roundOffError (Q.add a c)))
+		let e = (a +. c,  ( ea +. ec) +. (roundOffError (a +. c)))
 		in
-		let f = (Q.add b d, Q.add (Q.add eb ed) (roundOffError (Q.add b d)))
+		let f = (b +. d, (eb +. ed) +. (roundOffError (b +. d)))
 		in
 		Itv (e, f)
 
@@ -81,18 +81,18 @@ let sem_minus x y = match x, y with
   | Bot, _ -> Bot
   | _, Bot -> Bot
   |  Itv ((a, ea),(b,eb)), Itv ((c, ec),(d,ed))  -> 
-		let e = (Q.sub a d, Q.add (Q.sub ea ed) (roundOffError (Q.sub a d)))
+		let e = (a -. d, (ea -. ed) +. (roundOffError (a -. d)))
 		in
-		let f = ( Q.sub b c, Q.add (Q.sub eb ec) (roundOffError (Q.sub b c)))
+		let f = ( b -. c,  (eb -. ec) +. (roundOffError ( b -. c)))
 		in
 		Itv (e, f)
 let ourMul (x,ex) (y,ey) = 
-	(Q.mul x y, Q.add 
-		(Q.add (Q.mul ex y) (Q.mul ey x))
-		(Q.add (Q.mul ex ey) (roundOffError (Q.mul y x)))
+	(x *. y,  
+		(( ex *. y) +. (ey *. x)) +.
+		((ex *. ey) +. (roundOffError (y *. x)))
 	)
-let ourMin (x,ex) (y,ey) = if Q.leq x y then (x,ex) else (y,ey)
-let ourMax (x,ex) (y,ey) = if Q.geq x y then (x,ex) else (y,ey)
+let ourMin (x,ex) (y,ey) = if x <= y then (x,ex) else (y,ey)
+let ourMax (x,ex) (y,ey) = if  x >= y then (x,ex) else (y,ey)
 let sem_times x y = match x, y with
   | Bot, _ -> Bot
   | _, Bot -> Bot
@@ -106,7 +106,7 @@ let sem_times x y = match x, y with
     Itv (e, f)
 
 let ourDiv (x,ex) (y,ey) = ourMul (x,ex) 
-(Q.div (Q.of_float 1.) y,roundOffError (Q.div (Q.of_float 1.) y))
+( ( 1.) /. y,roundOffError ( ( 1.) /. y))
 
 let sem_div_real x y =
   match x, y with
@@ -125,7 +125,7 @@ let sem_div_real x y =
 let sem_div x y = sem_div_real x y
 
 
-let sem_geq0 = meet (Itv ((Q.of_int 0,Q.of_int 0), (Q.inf,Q.of_int 0)))
+let sem_geq0 = meet (Itv (( 0.,0.), (infinity, 0.)))
 
 let backsem_plus x y r = meet x (sem_minus r y), meet y (sem_minus r x)
 
@@ -134,7 +134,7 @@ let backsem_minus x y r = meet x (sem_plus y r), meet y (sem_minus x r)
 let backsem_times x y r =
   let backsem_times_left x y r =
     (* [contains_0 x] returns true iff the interval x contains 0 *)
-    let contains_0 x = meet x (Itv ((Q.of_int 0, Q.of_int 0),(Q.of_int 0,Q.of_int 0))) <> Bot in
+    let contains_0 x = meet x (Itv (( 0.,0.),( 0.,0.))) <> Bot in
     if contains_0 y && contains_0 r then
       x  (* When both y and r can be 0,
             x * y = r doesn't teach us anything about x. *)
